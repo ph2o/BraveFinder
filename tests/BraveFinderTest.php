c550a03d1fa0f10215ca401083f3c015aa95a2ed
ph2o <phuguenin@protonmail.com>
2019-12-09 22:17:40 +0100

    Ajout des tests

---
+++ tests/BraveFinderTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BraveFinderTest extends WebTestCase
+{
+    public function testBraveFinderIndex()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Accueil');
+    }
+
+    public function testBraveFinderIndexTitle()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('title', 'Brave Finder application');
+    }
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = static::createClient();
+        $client->request('GET', $url);
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function urlProvider()
+    {
+        yield ['/'];
+        // yield ['/candidate'];
+        // yield ['/practice'];
+        // yield ['/blog/category/fixture-category'];
+        // yield ['/archives'];
+        // ...
+    }
+}
---
+++ tests/SomeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SomeTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
